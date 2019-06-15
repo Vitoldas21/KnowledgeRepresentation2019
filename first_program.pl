b32a64bd92a8b179a71f687fef81470075a8ab2c
Vitoldas21 <48758322+Vitoldas21@users.noreply.github.com>
2019-06-15 12:13:44 +0200

    Create first_program.pl

---
+++ first_program.pl
@@ -0,0 +1,12 @@
+
+% This is my first Prolog Program
+
+% Logic
+% sun_sunning -> go_beach
+%      ^             ^
+%      |             |
+%  antecedent    consequent
+
+% Prolog
+go_beach :- sun_sunning.
+sun_sunning.
